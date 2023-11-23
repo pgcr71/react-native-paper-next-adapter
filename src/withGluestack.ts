f53fa9cd3e4cd62d42639a4824fa960819e408dc
pgcr71 <34126474+pgcr71@users.noreply.github.com>
2023-11-23 22:09:13 +0530

    Update withGluestack.ts

--- src/withGluestack.ts
+++ src/withGluestack.ts
@@ -1,123 +1,103 @@
 import {
-  checkIfWorkspace,
-  getDependenciesFromNodeModules,
-  getExactDependenciesFromNodeModules,
-} from './utils';
-const findWorkspaceRoot = require('find-yarn-workspace-root');
+    checkIfWorkspace,
+    getDependenciesFromNodeModules,
+    getExactDependenciesFromNodeModules,
+  } from './utils';
+  const findWorkspaceRoot = require('find-yarn-workspace-root');
+  
+  const gluestackDeps = [
+    'react-native-paper'
+  ];
+  
+  const reactNativeDeps = [
+    'react-native',
+    'react-native-web',
+    'react-native-svg',
+    'react-dom', 
+    'react-art'
+  ];
+  
+  export default function withGluestackUI(nextConfig: any = {}) {
+    const currDir = process.cwd();
+    let rootDependencyList = [];
+    try {
+      rootDependencyList = getDependenciesFromNodeModules(currDir, gluestackDeps);
+    } catch (e) {}
+  
+    let rootExactDependencyList = [];
+    try {
+      rootExactDependencyList = getExactDependenciesFromNodeModules(
+        currDir,
+        reactNativeDeps
+      );
+    } catch (e) {}
+  
+    const workspaceRoot = findWorkspaceRoot(currDir); // Absolute path or null
+    const metaWorkspace = checkIfWorkspace(currDir);
+    let parentDependencyList = [];
+    let parentExactDependencyList = [];
+  
+    if (metaWorkspace.isWorkspace) {
+      try {
+        parentDependencyList = getDependenciesFromNodeModules(
+          metaWorkspace.workspacePath,
+          gluestackDeps
+        );
+        parentExactDependencyList = getExactDependenciesFromNodeModules(
+          metaWorkspace.workspacePath,
+          reactNativeDeps
+        );
+      } catch (e) {}
+    }
 
-const gluestackDeps = [
-  '@gluestack-ui',
-  '@react-native-aria',
-  '@gluestack-style',
-  '@gluestack',
-  '@expo',
-  '@legendapp',
-  'expo-',
-];
-
-const reactNativeDeps = [
-  'react-native',
-  'react-native-web',
-  'react-native-svg',
-];
-
-export default function withGluestackUI(nextConfig: any = {}) {
-  const currDir = process.cwd();
-  let rootDependencyList = [];
-  try {
-    rootDependencyList = getDependenciesFromNodeModules(currDir, gluestackDeps);
-  } catch (e) {}
-
-  let rootExactDependencyList = [];
-  try {
-    rootExactDependencyList = getExactDependenciesFromNodeModules(
-      currDir,
-      reactNativeDeps
+    if (workspaceRoot) {
+      try {
+        parentDependencyList = getDependenciesFromNodeModules(
+          workspaceRoot,
+          gluestackDeps
+        );
+        parentExactDependencyList = getExactDependenciesFromNodeModules(
+          workspaceRoot,
+          reactNativeDeps
+        );
+      } catch (e) {}
+    }
+    let gluestackUITranspileModules = Array.from(
+      new Set([
+        ...rootDependencyList,
+        ...parentDependencyList,
+        ...rootExactDependencyList,
+        ...parentExactDependencyList,
+        ...(nextConfig.transpilePackages || []),
+      ])
     );
-  } catch (e) {}
-
-  const workspaceRoot = findWorkspaceRoot(currDir); // Absolute path or null
-  const metaWorkspace = checkIfWorkspace(currDir);
-  let parentDependencyList = [];
-  let parentExactDependencyList = [];
-
-  if (metaWorkspace.isWorkspace) {
-    try {
-      parentDependencyList = getDependenciesFromNodeModules(
-        metaWorkspace.workspacePath,
-        gluestackDeps
-      );
-      parentExactDependencyList = getExactDependenciesFromNodeModules(
-        metaWorkspace.workspacePath,
-        reactNativeDeps
-      );
-    } catch (e) {}
+  
+    const updatedNextConfig = {
+      ...nextConfig,
+      transpilePackages: gluestackUITranspileModules,
+      webpack: (config: any) => {
+        config = nextConfig.webpack ? nextConfig.webpack(config) : config;
+  
+        config.resolve.alias = {
+          ...(config.resolve.alias || {}),
+          'react-native$': 'react-native-web',
+        };
+  
+        config.resolve.extensions = [
+          '.web.js',
+          '.web.ts',
+          '.web.tsx',
+          ...config.resolve.extensions,
+        ];
+  
+        config.module.rules.push({
+          test: /\.ttf$/,
+          loader: 'url-loader',
+        });
+  
+        return config;
+      },
+    };
+  
+    return updatedNextConfig;
   }
-
-  // if (metaWorkspace.isWorkspace) {
-  //   parentDependencyList = getDependenciesFromNodeModules(
-  //     path.resolve(currDir, '..'),
-  //     ['@gluestack-ui', '@react-native-aria']
-  //   );
-  // }
-
-  // if (workspaceRoot) {
-  //   parentDependencyList = getDependenciesFromNodeModules(workspaceRoot, [
-  //     '@gluestack-ui',
-  //     '@react-native-aria',
-  //     '@legendapp',
-  //     '@expo/html-elements',
-  //     'gluestack',
-  //   ]);
-  // }
-  if (workspaceRoot) {
-    try {
-      parentDependencyList = getDependenciesFromNodeModules(
-        workspaceRoot,
-        gluestackDeps
-      );
-      parentExactDependencyList = getExactDependenciesFromNodeModules(
-        workspaceRoot,
-        reactNativeDeps
-      );
-    } catch (e) {}
-  }
-  let gluestackUITranspileModules = Array.from(
-    new Set([
-      ...rootDependencyList,
-      ...parentDependencyList,
-      ...rootExactDependencyList,
-      ...parentExactDependencyList,
-      ...(nextConfig.transpilePackages || []),
-    ])
-  );
-
-  const updatedNextConfig = {
-    ...nextConfig,
-    transpilePackages: gluestackUITranspileModules,
-    webpack: (config: any) => {
-      config = nextConfig.webpack ? nextConfig.webpack(config) : config;
-
-      config.resolve.alias = {
-        ...(config.resolve.alias || {}),
-        'react-native$': 'react-native-web',
-      };
-
-      config.resolve.extensions = [
-        '.web.js',
-        '.web.ts',
-        '.web.tsx',
-        ...config.resolve.extensions,
-      ];
-
-      config.module.rules.push({
-        test: /\.ttf$/,
-        loader: 'url-loader',
-      });
-
-      return config;
-    },
-  };
-
-  return updatedNextConfig;
-}
